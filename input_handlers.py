08a67fa2c55fcc6e7420cc179693ec4b5b2b1658
Carlos López <yo@carlop.es>
2018-09-08 18:33:49 +0200

    Doing (and taking) some damage

--- input_handlers.py
+++ input_handlers.py
@@ -1,13 +1,23 @@
 def handle_keys(user_input):
+    key_char = user_input.char
+
     # Movement keys
-    if user_input.key == 'UP':
+    if user_input.key == 'UP' or key_char == 'k':
         return {'move': (0, -1)}
-    elif user_input.key == 'DOWN':
+    elif user_input.key == 'DOWN' or key_char == 'k':
         return {'move': (0, 1)}
-    elif user_input.key == 'LEFT':
+    elif user_input.key == 'LEFT' or key_char == 'h':
         return {'move': (-1, 0)}
-    elif user_input.key == 'RIGHT':
+    elif user_input.key == 'RIGHT' or key_char == 'l':
         return {'move': (1, 0)}
+    elif key_char == 'y' or (user_input.key == 'UP' and user_input.key == 'LEFT'):
+        return {'move': (-1, -1)}
+    elif key_char == 'u' or (user_input.key == 'UP' and user_input.key == 'RIGHT'):
+        return {'move': (1, -1)}
+    elif key_char == 'b' or (user_input.key == 'DOWN' and user_input.key == 'LEFT'):
+        return {'move': (-1, 1)}
+    elif key_char == 'n' or (user_input.key == 'DOWN' and user_input.key == 'RIGHT'):
+        return {'move': (1, 1)}
 
     if user_input.key == 'ENTER' and user_input.alt:
         # ALt+Enter: toggle full screen
