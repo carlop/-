77d404dbf1fa43e02c471d9ddac33413986dddfc
Carlos López <yo@carlop.es>
2018-10-08 18:47:57 +0200

    Part 11

--- entity.py
+++ entity.py
@@ -6,18 +6,20 @@ class Entity:
     """
     A generic object to represent players, enemies, items, etc.
     """
-    def __init__(self, x, y, char, color, name, blocks=False, render_order=RenderOrder.CORPSE, fighter=None, ai=None, item=None, inventory=None):
-        self.x = x
-        self.y = y
-        self.char = char
-        self.color = color
-        self.name = name
-        self.blocks = blocks
+    def __init__(self, x, y, char, color, name, blocks=False, render_order=RenderOrder.CORPSE, fighter=None, ai=None, item=None, inventory=None, stairs=None, level=None):
+        self.x            = x
+        self.y            = y
+        self.char         = char
+        self.color        = color
+        self.name         = name
+        self.blocks       = blocks
         self.render_order = render_order
-        self.fighter = fighter
-        self.ai = ai
-        self.item = item
-        self.inventory = inventory
+        self.fighter      = fighter
+        self.ai           = ai
+        self.item         = item
+        self.inventory    = inventory
+        self.stairs       = stairs
+        self.level        = level
 
         if self.fighter:
             self.fighter.owner = self
@@ -30,6 +32,12 @@ class Entity:
 
         if self.inventory:
             self.inventory.owner = self
+
+        if self.stairs:
+            self.stairs.owner = self
+
+        if self.level:
+            self.level.owner = self
 
 
     def move(self, dx, dy):
